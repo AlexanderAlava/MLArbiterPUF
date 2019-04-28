b7052beec1bcf721f13d6036737fff213864d6b1
Alexander Alava <alexanderalava@Alexanders-MacBook-Pro-3.local>
2019-04-28 01:10:31 -0400

    Updating to branch out

--- CRPAttackModel6.py
+++ CRPAttackModel6.py
@@ -7,24 +7,15 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 #import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
 data = pd.read_csv("CRPSets.csv", header=None)
-sampled_data = data.sample(frac=0.66666667)
-
-
-
+sampled_data = data.sample(frac=0.5, random_state=1)
 train_data = sampled_data.drop(sampled_data.columns[[64]], axis=1).values
 train_responses = sampled_data[sampled_data.columns[[64]]].values
-
-
-#test_sampled_data = sampled_data.sample(frac=0.5)
-#test_train_data = test_sampled_data.drop(sampled_data.columns[[64]], axis=1).values
-#test_train_responses = test_sampled_data[sampled_data.columns[[64]]].values
-
-
 
 nonsampled_data = data.drop(sampled_data.index)
 test_data = nonsampled_data.drop(sampled_data.columns[[64]], axis=1).values
@@ -32,18 +23,26 @@ test_responses = nonsampled_data[sampled_data.columns[[64]]].values
 
 print(test_responses)
 
+
+#pca = PCA()
+#traind_pca = pca.fit(train_data)
+#trainr_pca = pca.fit(train_responses)
+
+#scores = pca.get_covariance()
+
+#for x in range(64):
+#    print(scores[x])
+
 # This is how the model is built
 model = keras.Sequential([
-    keras.layers.Dense(32, activation=tf.nn.relu, input_shape=(64,)),
+    keras.layers.Dense(32, activation=tf.nn.softmax, input_shape=(64,)),
     keras.layers.Dropout(0.1),
-    keras.layers.Dense(64, activation=tf.nn.relu),
+    keras.layers.Dense(64, activation=tf.nn.sigmoid),
     keras.layers.Dropout(0.2),
-    keras.layers.Dense(32, activation=tf.nn.relu),
+    keras.layers.Dense(32, activation=tf.nn.softmax),
     keras.layers.Dropout(0.1),
-    keras.layers.Dense(1, activation=tf.nn.relu)
+    keras.layers.Dense(1)
 ])
-
-#L1 and L2 regularization
 
 # This is where parameters of the model are described, we might not need any changes here
 model.compile(optimizer='adam',
